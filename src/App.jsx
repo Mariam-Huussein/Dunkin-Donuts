8464911ebd687549e39f2bbacc7386c1c8364d4d
Mariam-Huussein <mariamhusseen4@gmail.com>
2026-06-04 22:13:19 +0300

    fix: Build Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import HomePage from "./app/Pages/Home/HomePage";
-import AboutPage from "./app/Pages/About/AboutPage";
-import MenuPage from "./app/Pages/Menu/MenuPage";
-import ContactPage from "./app/Pages/Contact/ContactPage";
+import AboutPage from "./app/Pages/about/AboutPage";
+import MenuPage from "./app/Pages/menu/MenuPage";
+import ContactPage from "./app/Pages/contact/ContactPage";
 import Auth from "./app/Pages/Auth/AuthPage";
-import NotFound from "./app/Pages/NotFound/NotFound";
+import NotFound from "./app/Pages/notFound/NotFound";
 import Layout from "./app/Components/Layout/Layout";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store/store";
 import { Toaster } from "react-hot-toast";
 import AuthProvider from "./app/context/auth.context";
-import CartPage from "./app/Pages/Cart/CartPage";
-import WishlistPage from "./app/Pages/wishList/WishlistPage";
+import CartPage from "./app/Pages/cart/CartPage";
+import WishlistPage from "./app/Pages/WishList/WishlistPage";
 import GuestRoute from "./app/Components/Auth/GuestRoute/GuestRoute";
 
 export default function App() {
